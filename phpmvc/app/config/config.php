c44d4b015eaa5db6be589c7ed464e4a622e4c262
widyn84 <widyn84@gmail.com>
2022-10-14 01:05:09 +0700

    Menyelesaikan Video Database Wrapper

---
+++ phpmvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+define('BASEURL', 'http://localhost/prakweb_b_203040059/phpmvc/public');
+
+//DB
+define ('DB_HOST', 'localhost');
+define ('DB_USER', 'root');
+define ('DB_PASS', '');
+define ('DB_NAME', 'phpmvc');
+?>
--- phpmvc/app/core/Constants.php
+++
@@ -1,3 +0,0 @@
-<?php
-define('BASEURL', 'http://localhost/prakweb_b_203040059/phpmvc/public');
-?>
